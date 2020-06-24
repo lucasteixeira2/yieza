163dae022c811f68432d8a0617358c7aa79ea93b
Lucas Teixeira <lucasteixeira.dev@gmail.com>
2020-06-24 15:36:47 -0300

    Finalização do layout da página

--- js/custom.js
+++ js/custom.js
@@ -367,3 +367,17 @@ function closeNav() {
 }
 
 
+window.fbAsyncInit = function () {
+    FB.init({
+        xfbml: true,
+        version: 'v7.0'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/pt_BR/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
